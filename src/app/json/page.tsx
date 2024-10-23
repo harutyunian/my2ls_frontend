709bd910d340e06ffe50e1afa47d458470abf925
Arthur Harutyunyan <hayart98@gmail.com>
2024-10-23 17:49:51 +0400

    added simple variant of json formatter

---
+++ src/app/json/page.tsx
@@ -0,0 +1,9 @@
+import JsonFormatter from "@/app/json/json-formatter/components/JsonFormatter/JsonFormatter";
+
+
+export default function JsonPage(){
+    return <div>
+        This is jsofghfghfghgfhn page<JsonFormatter/>
+        asdsads
+    </div>
+}
