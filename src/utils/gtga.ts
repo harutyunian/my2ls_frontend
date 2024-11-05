fd1b6ad6e034cbbd1ed2e8677c85f115ee4d9227
Arthur Harutyunyan <hayart98@gmail.com>
2024-11-05 17:40:52 +0400

    added google analytics config

---
+++ src/utils/gtga.ts
@@ -0,0 +1,29 @@
+import { GA_TRACKING_ID } from "@/constant/google-analytics";
+
+export const pageview = (url: string) => {
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+};
+
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+}) => {
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+  }
+};
