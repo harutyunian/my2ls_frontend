cb2a74ee0f2823e0382b75e4e711cf4a7e112247
Arthur Harutyunyan <hayart98@gmail.com>
2024-11-14 17:41:45 +0400

    added analytics with nextjs documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header/Header";
+import GoogleAnalysticsConfig from "./components/GoogleAnalytics/GoogleAnalystics";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <GoogleAnalysticsConfig />
     </html>
   );
 }
