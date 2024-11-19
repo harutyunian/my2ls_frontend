8f4d0bf0d3dbc4279f369f2810c984e10bc99914
Arthur Harutyunyan <hayart98@gmail.com>
2024-11-19 19:08:39 +0400

    added initial json page

--- src/routes/rutes.ts
+++ src/routes/rutes.ts
@@ -1,17 +1,7 @@
 export const MAIN_ROUTES = [
   {
-    name: "Youtube",
-    path: "/somepath",
-    icon: "https://cdn-icons-png.flaticon.com/512/1384/1384060.png",
-  },
-  {
     name: "Json",
     path: "/json",
     icon: "https://static-00.iconduck.com/assets.00/json-icon-1500x2048-leitoong.png",
-  },
-  {
-    name: "aper",
-    path: "/aper",
-    icon: "https://static-00.iconduck.com/assets.00/json-icon-1500x2048-leitoong.png",
-  },
+  }
 ];
