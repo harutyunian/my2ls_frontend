e033118fd69f190350932eb0ac2e1c9ce5d03644
Arthur Harutyunyan <hayart98@gmail.com>
2024-11-22 16:48:09 +0400

    refactored small code to build app

--- src/app/helper/jsonToXml.ts
+++ src/app/helper/jsonToXml.ts
@@ -2,20 +2,22 @@ export function jsonToXml(
   obj: Record<string, string | number | boolean>,
   rootName: string = "root"
 ): string {
-  function createXmlNode(key: string, value: any): string {
-    if (typeof value === "object" && !Array.isArray(value) && value !== null) {
-      return `<${key}>${jsonToXml(value)}</${key}>`;
-    } else if (Array.isArray(value)) {
-      return value.map((item) => createXmlNode(key, item)).join("");
-    } else {
-      return `<${key}>${value}</${key}>`;
-    }
-  }
+
+  // function createXmlNode(key: string, value: any): string {
+  //   if (typeof value === "object" && !Array.isArray(value) && value !== null) {
+  //     return `<${key}>${jsonToXml(value)}</${key}>`;
+  //   } else if (Array.isArray(value)) {
+  //     return value.map((item) => createXmlNode(key, item)).join("");
+  //   } else {
+  //     return `<${key}>${value}</${key}>`;
+  //   }
+  // }
 
   let xml = "";
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      xml += createXmlNode(key, obj[key]);
+      // xml += createXmlNode(key, obj[key]);
+      xml += xml;
     }
   }
 
