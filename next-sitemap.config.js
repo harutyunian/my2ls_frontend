c63a35178f1da441356b5620e5782de600e4cce7
Arthur Harutyunyan <arharutyunyan@Arthurs-MacBook-Pro.local>
2025-02-09 17:57:04 +0400

    added sietmap test

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://my2ls.com", // Change to your domain
+    generateRobotsTxt: true, // Automatically generates robots.txt
+    sitemapSize: 5000, // Number of URLs per sitemap
+};
